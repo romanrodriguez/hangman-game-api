fa285788c737add8625752a555fc1bcded68af65
Roman <romanrodriguez@users.noreply.github.com>
2016-09-13 14:18:07 -0500

    refactor: cron job runs once a week and only if user email registered and a game is still active

--- main.py
+++ main.py
@@ -15,18 +15,20 @@ from models import Game
 class SendReminderEmail(webapp2.RequestHandler):
     def get(self):
         """Send a reminder email to each User with an email about games.
-        Called every hour using a cron job"""
+        Called every thursday at 17:00 using a cron job"""
         app_id = app_identity.get_application_id()
         users = User.query(User.email != None)
         for user in users:
-            subject = 'This is a reminder!'
-            body = 'Hello {}, try out Guess A Number!'.format(user.name)
-            # This will send test emails, the arguments to send_mail are:
-            # from, to, subject, body
-            mail.send_mail('noreply@{}.appspotmail.com'.format(app_id),
-                           user.email,
-                           subject,
-                           body)
+            active_games = Game.query(Game.user == user.key).filter(Game.game_over == False)
+            if active_games: # send them an email.
+                subject = 'This is a reminder!'
+                body = 'Hello {}, try out Guess A Number!'.format(user.name)
+                # This will send test emails, the arguments to send_mail are:
+                # from, to, subject, body
+                mail.send_mail('noreply@{}.appspotmail.com'.format(app_id),
+                               user.email,
+                               subject,
+                               body)
 
 
 class UpdateAverageMovesRemaining(webapp2.RequestHandler):
